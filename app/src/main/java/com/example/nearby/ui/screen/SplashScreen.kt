c8f8c7e4acc3aa3a79d97d184b3bf3de266683d3
Vinicius Costa <vinicius.marinho@ic.ufal.br>
2024-12-11 23:24:59 -0300

    feat: create welcome screen and its components

---
+++ app/src/main/java/com/example/nearby/ui/screen/SplashScreen.kt
@@ -0,0 +1,41 @@
+package com.example.nearby.ui.screen
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nearby.ui.theme.GreenLight
+
+import com.example.nearby.R
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(GreenLight)
+            .fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(id = R.drawable.img_logo_logo_logo_text),
+            contentDescription = "Nearby Logo",
+        )
+        Image(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            painter = painterResource(id = R.drawable.bg_splash_screen),
+            contentDescription = "Background Image",
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
---
+++ app/src/main/java/com/example/nearby/ui/screen/WelcomeScreen.kt
@@ -0,0 +1,47 @@
+package com.example.nearby.ui.screen
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+import com.example.nearby.ui.components.button.NearbyButton
+import com.example.nearby.ui.components.welcome.WelcomeContent
+import com.example.nearby.ui.components.welcome.WelcomeHeader
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(color = Color.White)
+            .fillMaxSize()
+            .padding(horizontal = 40.dp, vertical = 48.dp)
+            .verticalScroll(state = rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        WelcomeHeader()
+        WelcomeContent()
+        NearbyButton(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Começar",
+            onClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun WelcomeScreenPreview() {
+    WelcomeScreen()
+}
